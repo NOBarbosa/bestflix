13f35ca1c96f5371f65c12f5620842b78e1eb1d0
Natália Barbosa <nob.ufba@gmail.com>
2021-11-21 21:21:33 -0300

    estrutura do footer criada

--- bestflix/src/components/Footer/index.js
+++ bestflix/src/components/Footer/index.js
@@ -1,9 +1,17 @@
 import './style.css';
+import logo from '../../assets/logo.svg';
+
 
 function Footer(){
     return(
         <footer>
-            Footer
+            <div className="footer-left">
+                <img src={logo} alt="logo" />
+                <p>lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
+            </div>
+            <div className="footer-right">
+                <p>lorem ipsum</p>
+            </div>
         </footer>
     )
 }
